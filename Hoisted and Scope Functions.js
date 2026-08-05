4acaeeeb6afedcaa450f4ca9429855011cebfd72
Lavanlalith <lavanyavl2795@gmail.com>
2026-08-05 11:37:03 +0530

    Update Hoisted and Scope Functions.js

--- Hoisted and Scope Functions.js
+++ Hoisted and Scope Functions.js
@@ -1,6 +1,15 @@
+/*Assignment
+Declare a const name as browserVersion (global)
+Assign value as Chrome
+Create a function by name getBrowserVersion
+Create if condition inside function to check if brwoser is chrome, then
+Dclare a local variable (browserVersion) and print that variable inside function (outside block)
+call that function from the javascript
+*/
+
 // Const variable as global and var is global scope and used inside the function and if block
 //print undefined
-/* const browserVersion = 'Chrome';
+/* const browserVersion = 'Chrome'; //global variable
 
 function getBrowserVersion()
 {
@@ -18,14 +27,14 @@ Output - Undefined
 
 // Const variable as global and let is global scope and used inside the function and if block
 //print undefined
-const browserVersion = 'Chrome';
+const browserVersion = 'Chrome'; // global variable
 
 function getBrowserVersion()
 {
-    let safariBrowser = 'Safari';
+    let safariBrowser = 'Safari'; // local variable inside the function
     if(browserVersion == "Chrome")
     {
-        let browserVersion = "Chrome 120";
+        let browserVersion = "Chrome 120"; //local variable inside function and block
         console.log("Inside if block with var:", browserVersion);
     }
     console.log("Outside if block with var:", browserVersion);
