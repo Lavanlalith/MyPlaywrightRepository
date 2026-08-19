f3dcac369dcf53e725ecc641c26c09de6588153d
Lavanlalith <lavanyavl2795@gmail.com>
2026-08-19 07:05:18 +0530

    Add files via upload

---
+++ W1D2DataTypesAssignments.js
@@ -0,0 +1,23 @@
+/*Assignment Details:
+Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`, including an uninitialized variable.
+Assignment Requirements:
+userName // String
+organization // String
+contactNumber // Number
+knowsAutomation // Boolean
+usesPlaywright // Undefined (not assigned)*/
+
+let userName = 'Lavanya'
+console.log(`The datatype of userName is ${typeof userName}`);
+
+let organization = 'Tata Consultancy'
+console.log(`The datatype of organization is ${typeof organization}`);
+
+let contactNumber = 8122091917
+console.log(`The datatype of contactNumber is ${typeof contactNumber}`);
+
+let knowsAutomation = true
+console.log(`The datatype of knowsAutomation is ${typeof knowsAutomation}`);
+
+let usesPlaywright 
+console.log(`The datatype of usesPlaywright is ${typeof usesPlaywright}`);
